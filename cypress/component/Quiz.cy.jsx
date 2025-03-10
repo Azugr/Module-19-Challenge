d8091dc1b40ef4bf3d55a1c4d0fff9bd3ab3310c
Azu <azugr@outlook.com>
2025-03-09 21:02:06 -0600

    corrected quiz

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,57 +1,37 @@
 import React from "react";
-import Quiz from "../../client/src/components/Quiz"; 
+import Quiz from "../../client/src/components/Quiz";
 import { mount } from "cypress/react18";
 
-describe("Quiz Functionality", () => {
+describe("Quiz Component", () => {
   beforeEach(() => {
     cy.intercept("GET", "/api/questions/random", { fixture: "questions.json" }).as("fetchQuestions");
   });
 
-  it("displays the quiz welcome screen", () => {
-    cy.then(() => {
-      mount(<Quiz />);
-    });
+  it("renders the quiz start screen", () => {
+    mount(<Quiz />);
+    cy.contains("Welcome to the Tech Quiz!").should("exist");
+    cy.contains("Start Quiz").should("exist");
 
-    cy.contains("Welcome to the Tech Quiz!").should("be.visible");
-    cy.contains("Start Quiz").should("be.visible");
-
-    // Capture an image of the initial screen
-    cy.screenshot("quiz-home");
   });
 
-  it("begins the quiz when 'Start Quiz' is clicked", () => {
-    cy.then(() => {
-      mount(<Quiz />);
-    });
-
-    cy.get("button").contains("Start Quiz").click();
+  it("starts the quiz when clicking 'Start Quiz'", () => {
+    mount(<Quiz />);
+    cy.contains("Start Quiz").click();
     cy.wait("@fetchQuestions");
+    cy.contains("What is the capital of France?").should("exist");
 
-    cy.get("body").should("include.text", "Which of the following");
-
-    // Screenshot after quiz starts
-    cy.screenshot("quiz-in-progress");
   });
 
-  it("displays the final screen upon completing the quiz", () => {
-    cy.then(() => {
-      mount(<Quiz />);
-    });
-
-    cy.get("button").contains("Start Quiz").click();
+  it("shows the completion screen after finishing the quiz", () => {
+    mount(<Quiz />);
+    cy.contains("Start Quiz").click();
     cy.wait("@fetchQuestions");
 
-    cy.get("body").should("include.text", "Which of the following");
+    cy.contains("What is the capital of France?").should("exist");
 
-    // Select an answer (Assumes buttons contain answers)
-    cy.get("button").contains("except").click();
+    cy.contains("Paris").click(); 
 
-    // Confirm the quiz has been completed
-    cy.contains("Quiz Completed", { timeout: 10000 }).should("be.visible");
+    cy.contains("Quiz Completed", { timeout: 10000 }).should("exist");
 
-    // Take a screenshot of the completion message
-    cy.screenshot("quiz-finished");
   });
 });
-
-
